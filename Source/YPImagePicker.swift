7f4c588da33b589b79c1ba76c1ca884e9707d81f
Arian Amini <app@arianamini.de>
2019-12-19 16:42:59 +0100

    bug fix

--- Source/YPImagePicker.swift
+++ Source/YPImagePicker.swift
@@ -147,7 +147,7 @@ override open func viewDidLoad() {
         
         // If user has not customized the Nav Bar tintColor, then use black.
         if UINavigationBar.appearance().tintColor == nil {
-            UINavigationBar.appearance().tintColor = .ypLabel
+//            UINavigationBar.appearance().tintColor = .ypLabel
         }
     }
     
