0508b15dc1a4d1152d402260018cfaf40a753e2a
Arian Amini <app@arianamini.de>
2020-01-07 08:20:14 +0100

    bf

--- Source/Pages/Gallery/YPLibrary+LibraryChange.swift
+++ Source/Pages/Gallery/YPLibrary+LibraryChange.swift
@@ -16,37 +16,38 @@ extension YPLibraryVC: PHPhotoLibraryChangeObserver {
     
     public func photoLibraryDidChange(_ changeInstance: PHChange) {
         DispatchQueue.main.async {
-            let fetchResult = self.mediaManager.fetchResult!
-            let collectionChanges = changeInstance.changeDetails(for: fetchResult)
-            if collectionChanges != nil {
-                self.mediaManager.fetchResult = collectionChanges!.fetchResultAfterChanges
-                let collectionView = self.v.collectionView!
-                if !collectionChanges!.hasIncrementalChanges || collectionChanges!.hasMoves {
-                    collectionView.reloadData()
-                } else {
-                    collectionView.performBatchUpdates({
-                        let removedIndexes = collectionChanges!.removedIndexes
-                        if (removedIndexes?.count ?? 0) != 0 {
-                            collectionView.deleteItems(at: removedIndexes!.aapl_indexPathsFromIndexesWithSection(0))
-                        }
-                        let insertedIndexes = collectionChanges!.insertedIndexes
-                        if (insertedIndexes?.count ?? 0) != 0 {
-                            let indexPaths = insertedIndexes!.aapl_indexPathsFromIndexesWithSection(0)
-                            collectionView.insertItems(at: indexPaths)
-                            if let indexPath = indexPaths.first {
-                                self.select(at: indexPath)
+            if let fetchResult = self.mediaManager.fetchResult {
+                let collectionChanges = changeInstance.changeDetails(for: fetchResult)
+                if collectionChanges != nil {
+                    self.mediaManager.fetchResult = collectionChanges!.fetchResultAfterChanges
+                    let collectionView = self.v.collectionView!
+                    if !collectionChanges!.hasIncrementalChanges || collectionChanges!.hasMoves {
+                        collectionView.reloadData()
+                    } else {
+                        collectionView.performBatchUpdates({
+                            let removedIndexes = collectionChanges!.removedIndexes
+                            if (removedIndexes?.count ?? 0) != 0 {
+                                collectionView.deleteItems(at: removedIndexes!.aapl_indexPathsFromIndexesWithSection(0))
                             }
-                        }
-                    }, completion: { finished in
-                        if finished {
-                            let changedIndexes = collectionChanges!.changedIndexes
-                            if (changedIndexes?.count ?? 0) != 0 {
-                                collectionView.reloadItems(at: changedIndexes!.aapl_indexPathsFromIndexesWithSection(0))
+                            let insertedIndexes = collectionChanges!.insertedIndexes
+                            if (insertedIndexes?.count ?? 0) != 0 {
+                                let indexPaths = insertedIndexes!.aapl_indexPathsFromIndexesWithSection(0)
+                                collectionView.insertItems(at: indexPaths)
+                                if let indexPath = indexPaths.first {
+                                    self.select(at: indexPath)
+                                }
                             }
-                        }
-                    })
+                        }, completion: { finished in
+                            if finished {
+                                let changedIndexes = collectionChanges!.changedIndexes
+                                if (changedIndexes?.count ?? 0) != 0 {
+                                    collectionView.reloadItems(at: changedIndexes!.aapl_indexPathsFromIndexesWithSection(0))
+                                }
+                            }
+                        })
+                    }
+                    self.mediaManager.resetCachedAssets()
                 }
-                self.mediaManager.resetCachedAssets()
             }
         }
     }
